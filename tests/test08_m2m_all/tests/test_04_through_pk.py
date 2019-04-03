7c570f3d9e5c795ecb57c877b16d0a96d3a18d06
Michael Kim <mkim0407@gmail.com>
2019-04-03 11:52:58 -0700

    add save test

--- tests/test08_m2m_all/tests/test_04_through_pk.py
+++ tests/test08_m2m_all/tests/test_04_through_pk.py
@@ -17,3 +17,19 @@ def test_select():
 def test_select_unsupported():
     with pytest.raises(FieldError, match='Unsupported lookup'):
         t = Through.objects.get(pk__in=['1-4'])
+
+
+@pytest.mark.django_db
+def test_save():
+    assert not Through.objects.filter(a_id=2, b_id=3).exists()
+
+    t = Through(
+        a_id=2,
+        b_id=3,
+    )
+    t.save()
+
+    t = Through.objects.get(a_id=2, b_id=3)
+    assert t.primary_key == '2-3'
+    assert t.a_id == 2
+    assert t.b_id == 3
