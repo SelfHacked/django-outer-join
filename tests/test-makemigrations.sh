538a5e8fb5ac815ee56f7d22eb4813d0f20aa6a5
Michael Kim <mkim0407@gmail.com>
2019-04-03 14:49:32 -0700

    echo information in test makemigrations script

--- tests/test-makemigrations.sh
+++ tests/test-makemigrations.sh
@@ -5,6 +5,7 @@ django_version="$(python -c "import django; print(django.__version__[:3])")"
 if [[ "${django_version}" == "2.0" ]]
 then
     # django 2.0 requires NullBooleanField instead of BooleanField(null=True)
+    echo "Using django 2.0, skipping"
     exit 0
 fi
 
@@ -14,12 +15,16 @@ python manage.py makemigrations > ${result}
 code=$?
 if [[ ${code} != 0 ]]
 then
+    echo "makemigrations exited with ${code}"
     exit ${code}
 fi
 
-if [[ "$(cat ${result})" == "No changes detected" ]]
+expect="No changes detected"
+if [[ "$(cat ${result})" == "${expect}" ]]
 then
     exit 0
 else
+    echo "makemigrations is not clean (expect '${expect}'):"
+    cat ${result}
     exit 2
 fi
